37c9055e01c88c113a9e2cf7ac90410dd037c662
Abdul Moiz <moiza0799@gmail.com>
2021-12-05 19:34:42 +0500

    CLimbing leaderboard problem solved.

--- HackerRankProblemSolving/Program.cs
+++ HackerRankProblemSolving/Program.cs
@@ -10,6 +10,28 @@ namespace HackerRankProblemSolving
     {
         static void Main(string[] args)
         {
+            List<int> ranked = new List<int> { 100, 100, 50, 40, 40, 20, 10 };
+            List<int> player = new List<int> { 5, 25, 50, 120 };
+            climbingLeaderboard(ranked, player);
+        }
+        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+        {
+            ranked = ranked.Distinct().ToList();
+            List<int> ranks = new List<int>();
+            int count;
+            foreach (var item in player)
+            {
+                count = 1;
+                foreach (var r in ranked)
+                {
+                    if (item < r)
+                    {
+                        count++;
+                    }
+                }
+                ranks.Add(count);
+            }
+            return ranks;
         }
     }
 }
