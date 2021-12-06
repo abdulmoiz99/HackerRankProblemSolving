34d92301d5e86075a710e9ae923af8aeb0a99584
Abdul Moiz <moiza0799@gmail.com>
2021-12-06 21:07:25 +0500

    Flat Land Space Station

--- HackerRankProblemSolving/Program.cs
+++ HackerRankProblemSolving/Program.cs
@@ -10,28 +10,34 @@ namespace HackerRankProblemSolving
     {
         static void Main(string[] args)
         {
-            List<int> ranked = new List<int> { 100, 100, 50, 40, 40, 20, 10 };
-            List<int> player = new List<int> { 5, 25, 50, 120 };
-            climbingLeaderboard(ranked, player);
+            int[] c = {0,4 };
+            int n = 5;
+
+            Console.WriteLine(flatlandSpaceStations(n, c));
+
         }
-        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+        static int flatlandSpaceStations(int n, int[] c)
         {
-            ranked = ranked.Distinct().ToList();
-            List<int> ranks = new List<int>();
-            int count;
-            foreach (var item in player)
+            int max = 0;
+            if (n == c.Length) return 0;
+            int nearest = 0;
+            for (int i = 0; i < n; i++)
             {
-                count = 1;
-                foreach (var r in ranked)
+                if (c.Contains(i) && max <= 0)
                 {
-                    if (item < r)
+                    continue;
+                }
+                else
+                {
+                    nearest = c.OrderBy(x => Math.Abs((long)x - i)).First();
+                    nearest = Math.Abs(nearest - i);
+                    if (nearest > max)
                     {
-                        count++;
+                        max = nearest;
                     }
                 }
-                ranks.Add(count);
             }
-            return ranks;
+            return max;
         }
     }
 }
