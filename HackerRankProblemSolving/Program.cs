14911bfd47771a62193f81d580fac2ea75683279
Abdul Moiz <moiza0799@gmail.com>
2021-12-08 22:37:22 +0500

    Ceaser Cipher

--- HackerRankProblemSolving/Program.cs
+++ HackerRankProblemSolving/Program.cs
@@ -10,22 +10,37 @@ namespace HackerRankProblemSolving
     {
         static void Main(string[] args)
         {
-            string s = "12:01:00AM";
-           
-            Console.WriteLine(timeConversion(s));
+            string s = "Pz-/aI/J`EvfthGH";
+            //string s = "abc";
+            int k = 66;
+            Console.WriteLine(caesarCipher(s, k));
         }
-        public static string timeConversion(string s)
+        public static string caesarCipher(string s, int k)
         {
-            if (s.Substring(s.Length - 2) ==  "AM" && s.Substring(0, 2) == "12") 
+            char[] cipherText = s.ToCharArray();
+            char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+            int index = 0;
+            for (   int i = 0; i < cipherText.Length; i++)
             {
-                return "00" + s.Substring(2, s.Length - 4);
+                if (letters.Contains(Char.ToUpper(cipherText[i])))
+                {
+                    index = Array.IndexOf(letters, Char.ToUpper(cipherText[i]));
+                    //(x+k) mod 26
+                    index = (verifyIndex(index + k)) % 26;
+                    if (Char.IsUpper(cipherText[i])) cipherText[i] = letters[index];
+                    else cipherText[i] = Char.ToLower(letters[index]);
+                }
             }
-            else if (s.Substring(s.Length - 2) == "PM")
+            return new string(cipherText);
+        }
+        public static int verifyIndex(int sum)
+        {
+            while (true)
             {
-                int t = Convert.ToInt32(s.Substring(0, 2)) + 12;
-                return t + s.Substring(2, s.Length - 4); ;
+                if (sum >25) sum = sum - 26;
+                else break;
             }
-            return s;
+            return sum;
         }
     }
 }
