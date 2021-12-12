e97fe5e71689da945c506105531bf678c47ebf2c
Abdul Moiz <moiza0799@gmail.com>
2021-12-12 20:18:28 +0500

    Time Conversion Problem Solved

--- HackerRankProblemSolving/Program.cs
+++ HackerRankProblemSolving/Program.cs
@@ -10,33 +10,13 @@ namespace HackerRankProblemSolving
     {
         static void Main(string[] args)
         {
-            string s = "Pz-/aI/J`EvfthGH";
-            //string s = "abc";
-            int k = 66;
+            Console.WriteLine(timeConversion("07:05:45PM"));
         }
-        public static string kangaroo(int x1, int v1, int x2, int v2)
+        public static string timeConversion(string s)
         {
-            if (x2 > x1 && v2 > v1)
-            {
-                return "NO";
-            }
-            int pos1 = x1 + v1;
-            int pos2 = x2 + v2;
-
-            while (pos1 < 10000 && pos2 < 10000)
-            {
-                if (pos1 == pos2)
-                {
-                    return "YES";
-                }
-                else
-                {
-                    pos1 += v1;
-                    pos2 += v2;
-
-                }
-            }
-            return "NO";
+            DateTime dt = Convert.ToDateTime(s);
+            String time24 = dt.ToString("HH:mm:ss");
+            return time24;
         }
 
     }
